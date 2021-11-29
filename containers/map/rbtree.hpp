ed696c2121f85cb26148426484bd0da0795aeae2
ancoulon <ancoulon@student.s19.be>
2021-11-29 16:42:28 +0100

    implemented pairs and started map

---
+++ containers/map/rbtree.hpp
@@ -0,0 +1,17 @@
+#ifndef FT_RBTREE_HPP
+#define FT_RBTREE_HPP
+
+#include <functional>
+
+namespace ft
+{
+
+	template <class T, class Compare = std::less<T>, >
+	class RBTree
+	{
+
+	}
+
+}
+
+#endif
