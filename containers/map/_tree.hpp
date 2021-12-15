acb885279d874813ff15c3fedd321c4826d86ebe
ancoulon <ancoulon@student.s19.be>
2021-12-15 13:42:33 +0100

    tree erase()

--- containers/map/_tree.hpp
+++ containers/map/_tree.hpp
@@ -110,21 +110,7 @@ namespace ft
 
 		size_t erase(value_type key)
 		{
-			node_type * node = this->search(key)
-			if (node == nullptr)
-				return 0;
-
-			if (node->left == nullptr && node->right == nullptr)
-			{
-				this->_alloc.destroy(&node->value);
-				this->_node_alloc.deallocate(node, 1);
-			}
-			else if (node->right == nullptr)
-			{
-				node_type * tmp = node;
-				node = node->right;
-			}
-			
+			return this->_eraseNode(this->_root, key) != NULL;
 		}
 
 	private:
@@ -156,6 +142,45 @@ namespace ft
 				return this->_searchTree(node->left, key);
 			else
 				return this->_searchTree(node->right, key);
+		}
+
+		node_type * _eraseNode(node_type * node, value_type key)
+		{
+			if (node == nullptr)
+				return node;
+			else if (this->_comp(key.first, node->value.first))
+				node->left = this->_eraseNode(node->left, key);
+			else if (this->_comp(node->value.first, key.first))
+				node->right = this->_eraseNode(node->right, key);
+			else
+			{
+				if (node->left == nullptr && node->right == nullptr)
+				{
+					this->_alloc.destroy(&node->value);
+					this->_node_alloc.deallocate(node, 1);
+				}
+				else if (node->right == nullptr)
+				{
+					node_type * tmp = node;
+					node = node->left;
+					this->_alloc.destroy(&tmp->value);
+					this->_node_alloc.deallocate(tmp, 1);
+				}
+				else if (node->left == nullptr)
+				{
+					node_type * tmp = node;
+					node = node->right;
+					this->_alloc.destroy(&tmp->value);
+					this->_node_alloc.deallocate(tmp, 1);
+				}
+				else
+				{
+					node_type * tmp = this->_minimum(node->right);
+					node->value = tmp->value;
+					node->right = this->_eraseNode(node->right, tmp->value);
+				}
+			}
+			return node;
 		}
 
 	};
